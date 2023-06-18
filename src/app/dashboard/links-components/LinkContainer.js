a7d6ce4e9942875455706670eeccce8ec80efea2
AJ DeMarco <79189312+ajdemarco95@users.noreply.github.com>
2023-06-18 01:03:26 -0400

    Started conditional rendering

--- src/app/dashboard/links-components/LinkContainer.js
+++ src/app/dashboard/links-components/LinkContainer.js
@@ -1,11 +1,18 @@
 import LinkCard from "./LinkCard";
+import { useState } from "react";
 function LinkContainer(props) {
   const { linkArr } = props;
+  const [isEditing, setIsEditing] = useState(false);
 
   return (
     <div className="container">
       {linkArr.map((link, index) => (
-        <LinkCard key={index} link={link} />
+        <LinkCard
+          key={index}
+          link={link}
+          isEditing={isEditing}
+          setIsEditing={setIsEditing}
+        />
       ))}
       <button className="btn btn-primary btn-block mt-5">
         Add new link...
